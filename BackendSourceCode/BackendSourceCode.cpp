11165b337e5e157505b9bf83a2d015e4028a9014
Hooder <hoodernew@gmail.com>
2024-10-03 22:54:23 +1300

    Phssss I need a break

--- BackendSourceCode/BackendSourceCode.cpp
+++ BackendSourceCode/BackendSourceCode.cpp
@@ -1,28 +1,41 @@
 #include <iostream>
 
-void cardInformation()
-{
+
     struct cardProperties
     {
         std::string characterInformation;
         int HP;
         double MP;
         std::string characterSkill;
-    } characterobject1, characterobject2;
+    };
 
 
-    void callingCard(cardProperties CP)
+    cardProperties createCharacter() 
     {
-        characterobject1.characterInformation = "Tenth Doctor";
-        characterobject1.HP = 16;
+        cardProperties playerobj;
+        
+
+        playerobj.characterInformation = "Tenth Doctor";
+        playerobj.HP = 16;
+        playerobj.MP = 14.1;
+        playerobj.characterSkill = "Sonic Sheild:";
+
+        return playerobj;
     }
-}
+
+
+    void PlayerChoice() 
+    {
+        std::cout << "Select a Hero for this adventure!";
+
+        cardProperties obj1 = createCharacter(); // how is this connecting!?
+
+        std::cout << obj1.characterInformation;
+    }
 
 
 
-
-
-int main() // Game itself runs through here
+int main() 
 {
     
 }
