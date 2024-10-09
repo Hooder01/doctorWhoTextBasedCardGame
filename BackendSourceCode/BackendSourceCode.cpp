317cf9520a7f57bbc3d10adb7f899bba0e29de4e
Hooder <hoodernew@gmail.com>
2024-10-10 11:47:31 +1300

    StructVER
    I legit need to start this thing again!
    
    this is what happens when I get sick

--- BackendSourceCode/BackendSourceCode.cpp
+++ BackendSourceCode/BackendSourceCode.cpp
@@ -6,21 +6,27 @@
         std::string characterInformation;
         int HP;
         double MP;
-        std::string characterSkill;
+        std::string characterSkill; // this needs to be a seprate function
     };
 
 
-    cardProperties createCharacter() 
+    cardProperties createCharacter() //?????!!
     {
         cardProperties playerobj;
-        
+        cardProperties playerobj2;
 
         playerobj.characterInformation = "Tenth Doctor";
         playerobj.HP = 16;
         playerobj.MP = 14.1;
         playerobj.characterSkill = "Sonic Sheild:";
 
-        return playerobj;
+        playerobj2.characterInformation = "K9";
+        playerobj2.HP = 15;
+        playerobj2.MP = 15.5;
+        playerobj2.characterSkill = "Good Dog!:";
+
+            return playerobj;
+            
     }
 
 
@@ -28,9 +34,9 @@
     {
         std::cout << "Select a Hero for this adventure!";
 
-        cardProperties obj1 = createCharacter(); // how is this connecting!?
+         
 
-        std::cout << obj1.characterInformation;
+        
     }
 
 
