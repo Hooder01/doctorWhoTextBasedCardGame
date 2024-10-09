b4ca4626610f8b36015e7b79478cab91f57a5c14
Hooder <hoodernew@gmail.com>
2024-10-10 12:30:23 +1300

    RESTART V0.1

--- BackendSourceCode/BackendSourceCode.cpp
+++ BackendSourceCode/BackendSourceCode.cpp
@@ -1,39 +1,46 @@
 #include <iostream>
 
 
-    struct cardProperties
+class cardData 
+{
+    public:
+
+    std::string characterName;
+    int HP;
+    double MP;
+    
+    void characterSkill() 
     {
-        std::string characterInformation;
-        int HP;
-        double MP;
-        std::string characterSkill;
-    };
 
-
-    cardProperties createCharacter() 
-    {
-        cardProperties playerobj;
-        
-
-        playerobj.characterInformation = "Tenth Doctor";
-        playerobj.HP = 16;
-        playerobj.MP = 14.1;
-        playerobj.characterSkill = "Sonic Sheild:";
-
-        return playerobj;
     }
 
-
-    void PlayerChoice() 
+    cardData(std::string cn, int hp, double mp) // public constructor with paremeters
     {
-        std::cout << "Select a Hero for this adventure!";
-
-        cardProperties obj1 = createCharacter(); // how is this connecting!?
-
-        std::cout << obj1.characterInformation;
+        characterName = cn;
+        HP = hp;
+        MP = mp;
+        // assgined the new paremeter data names
     }
+};
 
 
+void characterInformation() 
+{
+    cardData cardInfo1("Tenth Doctor", 14, 15.1);
+    cardData cardInfo2("K9", 12, 17.1);
+}
+
+class gameLogic
+{
+    public:
+
+    /// should this be a class or a function!?
+
+    void playerStart() 
+    {
+        std::cout << "Please choose your hero!" << cardInfo1.HP; // why is this undifined
+    }
+};
 
 int main() 
 {
