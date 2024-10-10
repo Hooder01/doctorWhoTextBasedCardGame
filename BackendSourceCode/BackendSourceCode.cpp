d9135d426aeeca984fb6d52caa59cf83c9e64c5a
Hooder <hoodernew@gmail.com>
2024-10-11 12:24:46 +1300

    NEW V0.1.3
    Finally fixed the bugs but I need to somehow call the constructor objects before the two new void methods.

--- BackendSourceCode/BackendSourceCode.cpp
+++ BackendSourceCode/BackendSourceCode.cpp
@@ -31,24 +31,46 @@ cardData::cardData(std::string cn, int hp, double mp) // Constructor definition 
 class gameLogic
 {
 
-    void playbleChar() // <-- this should be called for player selection
+    void battleSystem(){}
+
+    void TDStory()
     {
-        cardData charInfo1("Tenth Doctor", 14, 16.1);
-        cardData charInfo2("K9", 12, 18.1);
+        std::cout << "TEST TEST!!";
     }
 
-  public: void onBeginPlay()
+
+
+  public: void onBeginPlay() // this is called into the main function first!
     {
-        std::cout << "DOCTOR WHO: The Ten Rings Of Garcorndith";
-        std::cout << "\n\nPlease select your Story: " << charInfo
+      cardData charInfo1("Tenth Doctor", 14, 16.1);
+      cardData charInfo2("K9", 12, 18.1);
+      // can we make these two sepreate somehow?
+
+      std::cout << "DOCTOR WHO: The Ten Rings Of Garcorndith";
         
+      int userStorySelection; // will promt user to select a number for the characters story
+
+      std::cout << "\n\nPlease Select Your Story!\n\n" << "\n\n1:" << charInfo1.characterName << "\n\n2:" << charInfo2.characterName;
+        
+      std::cin >> userStorySelection;
+
+      if (userStorySelection == 1)
+      {
+          TDStory(); // calls for the Tenth Doctor Side of the story
+      }
+      else if (userStorySelection == 2)
+      {
+          std::cout << "IN PRODUCTION!"; // TEMP!
+      }
     }
+
+        
 };
 
 int main()
 {
     gameLogic objTEMP;
-
+    // THIS WILL ALL CHANGE!
     objTEMP.onBeginPlay();
 }
 
