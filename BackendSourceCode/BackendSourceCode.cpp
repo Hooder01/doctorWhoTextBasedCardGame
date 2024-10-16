dd4633b879df1d5718c5150639257681cecedc87
Hooder <hoodernew@gmail.com>
2024-10-16 13:47:49 +1300

    With more fixed..
    more spawn

--- BackendSourceCode/BackendSourceCode.cpp
+++ BackendSourceCode/BackendSourceCode.cpp
@@ -28,20 +28,11 @@ cardData::cardData(std::string cn, int hp, double mp) // Constructor definition 
     // Acsess to these objects can be anywhere!
 }
 
-class monsterData 
+/*class monsterData
 {
     public:
 
-    void hoodedFigure()
-    {
-        // would like to add some sorta art here in future
-
-        cardData MonsterInfo1("[Unknown Hooded Figure]", 5, 10.1);
-
-        std::string healthBar = "      [-----]        ";
-
-        std::cout << MonsterInfo1.characterName << "\n" << healthBar; // monsters like this will display in UI just above the UIDisplay array!
-    }
+    
 
     void dalek()
     {
@@ -53,7 +44,7 @@ class monsterData
 
         std::cout << MonsterInfo2.characterName << "\n" << healthBar;
     }
-};
+}; still want to use this class?*/
 
 class storyMiniGames 
 {
@@ -66,7 +57,7 @@ class storyMiniGames
         double playerselect;
         int numberArray[4]{ 11, 12, 13, 14 };
 
-        std::cout << "\n\nVOID SEQUENCER SET: "; std::cin >> playerselect;
+        std::cout << "\n\nVOID SEQUENCER SET: "; std::cin >> playerselect; // this needs fixed!!
         
         
         
@@ -81,16 +72,29 @@ class gameLogic
 {
     public:
 
+   void hoodedFigure()
+   {
+            // would like to add some sorta art here in future
+
+            cardData MonsterInfo1("[Unknown Hooded Figure]", 5, 10.1);
+
+            std::string healthBar = "      [-----]        ";
+
+            std::cout << MonsterInfo1.characterName << "\n" << healthBar; // monsters like this will display in UI just above the UIDisplay array!
+   }
+
+
     void battleSystem() // being public may fix this!?
     {
         cardData charInfo1("Tenth Doctor", 14, 16.1);
         cardData charInfo2("K9", 12, 18.1); // Do these need to be in any other class?
+        
 
         int callTurn = 1;
         std::string userTutChoice;
         int userSelect; // used or UIDisplay and other
         
-        std::array UIDisplay[3]{ "1: Attack", "2. Defend", "3. Stats" };// Array for the use of the battle menu UI 
+        std::string UIDisplay[3]{ "1: Attack", "2. Defend", "3. Stats" };// Array for the use of the battle menu UI 
         std::string beginBattle = "[BATTLE BEGIN!]"; // called every battleScenario()
         
 
@@ -100,18 +104,27 @@ class gameLogic
 
         if (userTutChoice == "y") 
         {
-            std::cout << "(below is the Turn System)" << "\n\n" << UIDisplay[0] << " <-- (Use this to do a simple melee attack, more options will open later!)" << "\n" << UIDisplay[1]; // finish this! <--
+            std::cout << "(below is the Turn System)" << "\n\n" << UIDisplay[0] << " <-- (Use this to do a simple melee attack, more options will open later!)" << "\n" << UIDisplay[1] << "(<-- this will defend you but ceartin attacks will still cause damage)" << "\n" << UIDisplay[2] << "(Check health, mana and alignment path)" << std::endl;
         }
         else 
         {
-            monsterData temp;
-            std::cout << beginBattle << "\n\n"; temp.hoodedFigure();
+            
+            std::cout << beginBattle << "\n\n" << 
 
-            std::cout << UIDisplay[0] << "\n" << UIDisplay[1] << "\n" << UIDisplay[2];
+            std::cout << "\n\n" << UIDisplay[0] << "\n" << UIDisplay[1] << "\n" << UIDisplay[2];
 
             std::cin >> userSelect;
+            
 
-            if (userSelect == UIDisplay[0]) 
+            if(userSelect == 1)
+            {
+               
+            }
+            if (userSelect == 2) 
+            {
+
+            }
+            else if(userSelect == 3)
             {
 
             }
@@ -140,7 +153,7 @@ class gameLogic
         std::cout << "(The blue outshape of the TARDIS spins around and around inside of the space time vortex)" << std::endl;
         std::cout << "The Doctor (you) look at the TARDIS control panel with gloomy face \n\n\n you sigh loudly to yourself \n\n\n'I'm getting too old for this..'";
         std::cout << "you get back your focus and hit a switch (beep beep!) \n\n'that's odd..'  \n you crouch to the TARDIS scanner \n\n\n'the Void Sequencer needs reset?'\n\n" << std::endl;
-        storyCall.TARDISMini1();
+        storyCall.TARDISMini1(); // calling first mini game
 
         std::cout << "\n\n\n\n\n(BANG BANG!!) sparks fly from the TARDIS console and you fall to the ground!";
         std::cout << "\n\n'THE KEY OF THE MARK HAS BEEN FOUND!'\n\n You stagger yourself up back to the console and see a black ghostly hooded figure";
@@ -149,28 +162,30 @@ class gameLogic
         std::cin >> dialogueChoice;
 
         // FIX THIS ARRAY!
-        /*if ()
+        if (dialogueChoice == "a")
         {
-            std::cout << dialogueOption[0] << "How has this strange hooded figure entered your TARDIS?";
+            std::cout << "\n" << dialogueOption[0] << "How has this strange hooded figure entered your TARDIS?";
 
             neturalPath++; // Path is incrmented by 1
         }
-        else if (dialogueChoice == dialogueOption[1]) 
+        else if (dialogueChoice == "b")
         {
-            std::cout << dialogueOption[1] << "'Listen.. I'm not a big fan of vistors who may I add,don't even send a post card first!'";
+            std::cout << "\n" << dialogueOption[1] << "'Listen.. I'm not a big fan of vistors who may I add,don't even send a post card first!'";
 
             lightPath++;
         }
-        else if (dialogueChoice == dialogueOption[2]) 
+        else if (dialogueChoice == "c")
         {
-            std::cout << dialogueOption[2] << "'I hope you're scared cause today is not the day to be messing with me. Why are you here!?";
+            std::cout << "\n" << dialogueOption[2] << "'I hope you're scared cause today is not the day to be messing with me. Why are you here!?";
 
             darkPath++;
-        }*/
+        }
 
 
         std::cout << "\n\n'ENOUGH!! THE KEY WILL COME WITH ME!'" << "\n\nYou need to defend yourself! (you grab an old wrench sitting below the console)" << "\n\n'Sorry but the key isn't opening it's doors any longer!'" << std::endl;
+        gameLogic tempobj;
 
+        tempobj.battleSystem();
     }
 
   public: void onBeginPlay() // this is called into the main function first!
@@ -205,11 +220,11 @@ class gameLogic
 
 int main()
 {
-    gameLogic TEMP;
+    gameLogic tempObj;
 
-    TEMP.battleSystem();
+    tempObj.TDStory();
 
-    // currently only used for debugging! 
+    
 }
 
 
